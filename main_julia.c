7d55828a3cf8068a1284c82cbd2ff20a8c6ed434
Julia BATORO <jbatoro@e1r4p9.clusters.42paris.fr>
2022-05-15 18:40:57 +0200

    julia : test fct search quotes

--- main_julia.c
+++ main_julia.c
@@ -4,16 +4,30 @@
 #include <readline/history.h>
 
 
+
+/* Check if there are quotes */
+void ft_search_quotes(char *str)
+{   
+    if (ft_strchr(str, '"') || ft_strchr(str, '\''))
+        ft_printf("String contains dquote or squote");
+    else
+        ft_printf("String no contains quote");
+    exit(EXIT_FAILURE);
+}
+
 /*Fonction qui va initialiserla structure */
 
 // EXEC 
 int main(int ac, char **av)
 {
-    const char *ptr;
-    while (temp == 0)
+    char *ptr = NULL;
+    int temp = -1;
+    while (temp <= 0)
     {
         ft_printf("minishell>");
         ptr = readline(ptr);
+        ft_search_quotes(ptr);
+        temp++;
     }
     return (0);
  // boucle infinie : lance le script   
--- src/lexer.c
+++ src/lexer.c
@@ -3,42 +3,35 @@
 /*----------------------------- LEXER ------------------------------*/
 
 /*******************************************************************/
-/* 
-- Récuperer la string entree en input avec readline
+
+/*- Récuperer la string entree en input avec readline
 - FIRST CHECK : if there are squotes || dquotes 
 
-////////////////////////////////////////////////////////////
-
 Si dquote ou squote :
-    -> Si squote ou dquote non fermées : voir ce qu' on fait (ammon)
+    -> Si squote ou dquote non fermées : erreur
     -> Parser et checker si on trouve des pipes au milieu de squotes || dquotes
         -> Si dquote :
-            -> tout ce qui est entre dquote == token (a voir si $ check mtn ou + tard)
+            -> tout ce qui est entre dquote == token 
         -> Si squote : tout ce qui est entre squotes == token 
-    -> Parser et chequer si on trouve des <><<>> au milieu des squotes || dquotes
+    -> Parser et chequer si on trouve des < >  << >> au milieu des squotes || dquotes
         -> Si dquote || squote :
                 -> le resultat == token + dquotes
                 -> le resultat == token
 Si absence de squote ou dquote :
     -> Parser et découper l'input en fonction des pipes.
 
-//////////////////////////////////////////////////////////////
+Si il n'y a pas de pipes
+
 
 TOKEN == mots qui seront interprétés plus tard.
-TOCKENS stockés dans un char** (ft_get_tokens).
+TOCKENS stockés dans un char** (ft_get_tokens).*/
 
 /********************************************************************/
-
-/* Check if there are quotes */
-while (str[i])
-
 
 char**    ft_get_tokens(char *str)
 {
     // get ride of isspace at the beggining && and the end of the string 
-    // ++ get ride of isspace at the beggining and at the end of each tokens
     // -> str_trim ? 
     // Handle multiple pipes
+    return (NULL);
 }
-
-check if there are pipe into quote 
