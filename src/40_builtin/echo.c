6ddc4922bd9b746346bc2b005d0f275afa803c72
Yann BELLOT <ybellot@e1r6p1.clusters.42paris.fr>
2022-05-21 20:56:55 +0200

    yann: update echo pour flag -nn...n et ajouts de tests

--- src/40_builtin/echo.c
+++ src/40_builtin/echo.c
@@ -6,18 +6,33 @@
 /*   By: ybellot <ybellot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 16:25:25 by ybellot           #+#    #+#             */
-/*   Updated: 2022/05/21 16:45:25 by ybellot          ###   ########.fr       */
+/*   Updated: 2022/05/21 20:49:43 by ybellot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// check if str == "-n"
+// check if str == "-n" || "-nn..n"
 static int	ft_is_n_flag(char *str)
 {
-	if (ft_strlen(str) == 2)
-		if (str[0] == '-' && str[1] == 'n')
+	int	i;
+	int	flag_n_found;
+
+	if (!str)
+		return (0);
+	i = 0;
+	flag_n_found = 0;
+	if (str[0] == '-')
+	{
+		i = 1;
+		while (str[i] == 'n')
+		{
+			flag_n_found = 1;
+			i++;
+		}
+		if (str[i] == '\0' && flag_n_found)
 			return (1);
+	}
 	return (0);
 }
 
@@ -41,52 +56,19 @@ static void	ft_print_arguments_list(char **arguments_list)
 
 void	ft_echo(char **arguments_list)
 {
-	int	_flag_found;
+	int	n_flag_found;
 	int	print_begin;
 
 	if (!arguments_list || !arguments_list[0])
 		return ;
-	if (ft_is_n_flag(arguments_list[0]))
+	n_flag_found = 0;
+	print_begin = 0;
+	while (ft_is_n_flag(arguments_list[print_begin]))
 	{
 		n_flag_found = 1;
-		print_begin = 1;
-	}
-	else
-	{
-		n_flag_found = 0;
-		print_begin = 0;
+		print_begin += 1;
 	}
 	ft_print_arguments_list(&(arguments_list[print_begin]));
 	if (!n_flag_found)
 		ft_printf("\n");
 }
-
-// version coherente avec zsh
-// void    ft_echo(char **arguments_list)
-// {
-//     int n_flag_found;
-//     int first_arg;
-//     int i;
-
-//     if (!arguments_list)
-//         return;
-//     n_flag_found = 0;
-//     first_arg = 1;
-//     i = 0;
-//     while (arguments_list[i])
-//     {
-//         if (!n_flag_found && ft_is_n_flag(arguments_list[i]))
-//             n_flag_found = 1;
-//         else
-//         {
-//             if (first_arg)
-//                 first_arg = 0;
-//             else
-//                 ft_printf(" ");
-//             ft_printf("%s", arguments_list[i]);
-//         }
-//         i++;
-//     }
-//     if (!n_flag_found)
-//         ft_printf("\n");
-// }
--- src/70_debug/test_echo.c
+++ src/70_debug/test_echo.c
@@ -1,40 +1,73 @@
 #include "minishell.h"
 
+
+// comportement attendu
+// 1er paramettre peut c
 void    ft_test_echo()
 {
-    char *arg1[5] = {"1ola", "como", "estas", "!",NULL};
+	ft_printf("test 1 : --->");
+    char *arg1[5] = {"ola", "como", "estas", "!",NULL};
     ft_echo(arg1);
-    ft_printf("--- \n");
+    ft_printf("<--- \n");
 
-    char *arg2[6] = {"2ola", "-n", "como", "estas", "!",NULL};
+	ft_printf("test 2 : --->");
+
+    char *arg2[6] = {"ola", "-n", "como", "estas", "!",NULL};
     ft_echo(arg2);
-    ft_printf("--- \n");
+    ft_printf("<--- \n");
 
-    char *arg3[7] = {"3ola", "como", "-n", "estas", "-n", "!",NULL};
+	ft_printf("test 3 : --->");
+    char *arg3[7] = {"ola", "como", "-n", "estas", "-n", "!",NULL};
     ft_echo(arg3);
-    ft_printf("--- \n");
+    ft_printf("<--- \n");
 
-    char *arg4[7] = {"4ola", "como", "-n", "estas", "!", "-n",NULL};
+	ft_printf("test 4 : --->");
+    char *arg4[7] = {"ola", "como", "-n", "estas", "!", "-n",NULL};
     ft_echo(arg4);
-    ft_printf("--- \n");
+    ft_printf("<--- \n");
 
-    char *arg5[8] = {"-n", "5ola", "como", "-n", "estas", "!", "-n",NULL};
+	ft_printf("test 5 : --->");
+    char *arg5[8] = {"-n", "ola", "como", "-n", "estas", "!", "-n",NULL};
     ft_echo(arg5);
-    ft_printf("-- \n");
+    ft_printf("<-- \n");
 
-    char *arg6[8] = {"-n", "-n", "6ola", "como", "-n", "estas", "!", NULL};
+	ft_printf("test 6 : --->");
+    char *arg6[8] = {"-n", "-n", "ola", "como", "-n", "estas", "!", NULL};
     ft_echo(arg6);
-    ft_printf("-- \n");
+    ft_printf("<-- \n");
 
+	ft_printf("test 7 : --->");
     char *arg7[2] = {"-n", NULL};
     ft_echo(arg7);
-    ft_printf("-- \n");
+    ft_printf("<-- \n");
 
+	ft_printf("test 8 : --->");
     char *arg8[1] = {NULL};
     ft_echo(arg8);
-    ft_printf("-- \n");
+    ft_printf("<-- \n");
 
-    char *arg9[2] = {"-na", NULL};
+	ft_printf("test 9 : --->");
+    char *arg9[2] = {"-", NULL};
     ft_echo(arg9);
-    ft_printf("-- \n");
+    ft_printf("<-- \n");
+
+	ft_printf("test 10 : --->");
+	char *arg10[2] = {"-na", NULL};
+    ft_echo(arg10);
+    ft_printf("<-- \n");
+
+	ft_printf("test 11 : --->");
+	char *arg11[2] = {"-nnnnnn", NULL};
+    ft_echo(arg11);
+    ft_printf("<-- \n");
+
+	ft_printf("test 12 : --->");
+	char *arg12[3] = {"-n", "-nnn", NULL};
+    ft_echo(arg12);
+    ft_printf("<-- \n");
+
+	ft_printf("test 13 : --->");
+	char *arg13[3] = {"-n", "-nnns", NULL};
+    ft_echo(arg13);
+    ft_printf("<-- \n");
 }
