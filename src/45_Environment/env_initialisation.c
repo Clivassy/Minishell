e85911d51b985fae52667f6ca6bce9b25f753884
Yann BELLOT <ybellot@e1r3p20.42.fr>
2022-06-04 16:49:25 +0200

    yann: reorganisation fichier env, passage define color dans .h

--- src/45_Environment/env_initialisation.c
+++ src/45_Environment/env_initialisation.c
@@ -1,25 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env_manipulation.c                                 :+:      :+:    :+:   */
+/*   env_initialisation.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybellot <ybellot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/23 17:17:43 by ybellot           #+#    #+#             */
-/*   Updated: 2022/05/23 17:59:43 by ybellot          ###   ########.fr       */
+/*   Created: 2022/06/04 14:42:18 by ybellot           #+#    #+#             */
+/*   Updated: 2022/06/04 14:42:19 by ybellot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "minishell.h"
 
-int nb_of_env_lines(char **envp)
+void    dup_env(t_data *data, char **envp)
 {
     int i;
 
+    data->env = malloc(sizeof(char *) * nb_of_env_lines(envp) + 1);
+    printf("nb lines = %d\n", nb_of_env_lines(envp));
+    i = 0;
+    while (i < nb_of_env_lines(envp) + 1)
+    {
+        data->env[i] = NULL;
+        i++;
+    }
     i = 0;
     while (envp[i])
+    {
+        data->env[i] = ft_strdup((const char *)envp[i]);
         i++;
-    return (i);
+    }
+    print_env(data->env);
 }
-
 
