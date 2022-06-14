650c43308b9907118804d54cc8165131567e00a5
Yann BELLOT <ybellot@e1r6p3.42.fr>
2022-06-14 22:19:47 +0200

    yann: save travail expand

--- src/25_expand/expand.c
+++ src/25_expand/expand.c
@@ -77,8 +77,9 @@ int	ft_size_after_expand(t_data *data, char *str)
 
 				ft_printf("key trouvee: %s \n", word);
 				ft_printf("len mot: %d \n", ft_strlen(ft_env_get_value(data, word)));
-
+				ft_printf("count avant: %d \n", count);
 				count += ft_strlen(ft_env_get_value(data, word));
+				ft_printf("count apres: %d \n", count);
 				i += ft_strlen(ft_env_get_value(data, word));
 				ft_printf("i:%d, str[i] = %c \n",i, str[i]);
 				ft_printf("len: %zu \n", ft_strlen(word));
@@ -102,6 +103,8 @@ int	ft_size_after_expand(t_data *data, char *str)
 		{
 			i++;
 			count++;
+			ft_printf("count add space: str[i]=%c \n", str[i]);
+
 		}
 		else
 		{
@@ -109,8 +112,10 @@ int	ft_size_after_expand(t_data *data, char *str)
 			{
 				i++;
 				count++;
+				ft_printf("count add trim: str[i]=%c \n", str[i]);
 			}
 		}
+		i++;
 	}
 	return (count);
 }
