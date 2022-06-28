af127c200d924f88ab43b9a98b29f663a4251d11
Julia Batoro <juliabatoro@MacBook-Pro-de-Julia.local>
2022-06-28 20:43:29 +0200

    parser

--- src/21_parser/parser.c
+++ src/21_parser/parser.c
@@ -47,30 +47,6 @@ int ft_redirect_errors(t_token *token)
     return (0);
 }
 
-/*int ft_redirect_errors(t_token *token)
-{
-    while (token)
-    {
-        if (ft_is_redirect_err(token))
-        {
-            if (!token->next)
-                ft_lexer_error("Error 5: nothing after redirect");
-            if (token->next->type == T_WORD)
-            {
-                if (!token->next->next)
-                    return(0);
-            }
-            if (!token->next->next && token->next->type != T_WORD)
-                ft_lexer_error("Error 6: no word after redirect");
-            if (token->next->next->type != T_WORD)
-                ft_lexer_error("Error 7: multiples redirect");
-            }
-        token = token->next;
-    }
-   // printf("NO REDIRECT ERRORS\n");
-    return (0);
-}*/
-
 int ft_is_empy_pipe(t_token *token)
 {
     if (!token->next && token->type != T_WORD)
@@ -83,6 +59,52 @@ int ft_is_empy_pipe(t_token *token)
     }
     return (0);
 }
+
+int ft_pipe_errors(t_token *token)
+{
+    //printf("first token is : %d\n", token->type);
+    while (token)
+    {
+        if (token->type == T_PIPE)
+        {
+            if (!token->next)
+                ft_lexer_error("Error 2 : pipe at the end");
+            if (token->next->type == T_PIPE)
+                ft_lexer_error("Error 3 : multiple pipes ");
+            if (token->next->type != T_WORD)
+            {
+                if (token->next->type != T_SPACE)
+            } 
+                && token->next->next->type == T_SPACE)
+                {
+                    if (!token->next->next->next)
+                        ft_lexer_error("Error : space and nothing after last redirect");
+                }
+            token = token->next;
+            if (ft_is_empy_pipe(token) == 0 )
+                ft_lexer_error("Error 4: no word found");
+        }
+        token = token->next;
+    }
+   // printf("NO PIPE ERRORS\n");
+    return(0);
+}
+
+int ft_parser(t_data *data)
+{
+    t_token *list;
+
+    list = data->tokens_list;
+    if (list->type == T_PIPE)
+        ft_lexer_error("Error 1 : pipe at the begining");
+    ft_pipe_errors(list);
+    ft_redirect_errors(list);
+    return (0);
+}
+
+
+
+/* SAVE 
 
 int ft_pipe_errors(t_token *token)
 {
@@ -111,14 +133,28 @@ int ft_pipe_errors(t_token *token)
     return(0);
 }
 
-int ft_parser(t_data *data)
+int ft_redirect_errors(t_token *token)
 {
-    t_token *list;
-
-    list = data->tokens_list;
-    if (list->type == T_PIPE)
-        ft_lexer_error("Error 1 : pipe at the begining");
-    ft_pipe_errors(list);
-    ft_redirect_errors(list);
+    while (token)
+    {
+        if (ft_is_redirect_err(token))
+        {
+            if (!token->next)
+                ft_lexer_error("Error 5: nothing after redirect");
+            if (token->next->type == T_WORD)
+            {
+                if (!token->next->next)
+                    return(0);
+            }
+            if (!token->next->next && token->next->type != T_WORD)
+                ft_lexer_error("Error 6: no word after redirect");
+            if (token->next->next->type != T_WORD)
+                ft_lexer_error("Error 7: multiples redirect");
+            }
+        token = token->next;
+    }
+   // printf("NO REDIRECT ERRORS\n");
     return (0);
 }
+
+*/
