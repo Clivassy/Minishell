90e0c2e7bb394043bda8431114c348f26852a889
Julia BATORO <jbatoro@e1r5p23.42.fr>
2022-07-07 21:42:00 +0200

    modif signal julia

--- src/24_signals/signals.c
+++ src/24_signals/signals.c
@@ -26,9 +26,9 @@ void    ft_handle_heredoc_signal(int signal)
 /* handle ctrl -c*/
 void ft_handle_sigint(int signal)
 {
+    ft_printf("\n");
     if (signal == SIGINT)
     {
-        ft_printf("\n");
         rl_on_new_line();
         rl_replace_line("", 0);
         rl_redisplay();
