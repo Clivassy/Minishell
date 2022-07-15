b4c43e473edb5fb8482e64058256c20eaddf9787
Yann BELLOT <ybellot@e1r5p20.clusters.42paris.fr>
2022-07-15 13:39:49 +0200

    corrections messages erreur

--- src/17_error_management/print_error_open.c
+++ src/17_error_management/print_error_open.c
@@ -2,7 +2,7 @@
 
 void	ft_print_open_error(t_data *data, char *file_name)
 {
-	ft_putstr_fd("bash: ", STDERR_FILENO);
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	ft_putstr_fd(file_name, STDERR_FILENO);
 	ft_putstr_fd(": ", STDERR_FILENO);
 	perror(NULL);
